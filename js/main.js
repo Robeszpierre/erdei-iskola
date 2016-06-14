afda22b5b7f96cb5c92bf96129ee0a1d6367c983
Robeszpierre <robeszpierre@gmail.com>
2016-06-14 02:45:26 +0200

    introduce

--- js/main.js
+++ js/main.js
@@ -1,1 +1,3 @@
-$('.summer-wrapper').parallax({imageSrc: 'img/background2.jpg'});
+$('.parallax-window').parallax({imageSrc: 'img/background.jpg'});
+$('.parallax-window2').parallax({imageSrc: 'img/background2.jpg'});
+$('.parallax-window3').parallax({imageSrc: 'img/background3.jpg'});
