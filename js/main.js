10fe97759b46956be569c7f8d38f1faada10bf6f
Robeszpierre <robeszpierre@gmail.com>
2016-06-16 02:11:40 +0200

    navbar and scroll animations

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 $('.parallax-window').parallax({imageSrc: 'img/background.jpg'});
 $('.parallax-window2').parallax({imageSrc: 'img/background2.jpg'});
 $('.parallax-window3').parallax({imageSrc: 'img/background3.jpg'});
+
+$('body').scrollspy({ target: '#navbar', offset: 51 });
+
+ $('#navbar').scrollToFixed({
+      });
+
+      /* smooth scrolling for scroll to top */
+      $('.navbar-brand').click(function(e){
+                 e.preventDefault();
+               $('body,html').animate({scrollTop:0},1000);
+             });
+
+               /*smooth scrolling for nav sections */
+              $('#navbar li>a').click(function(e){
+                  $('#nav li>a').removeClass('active');
+                  var link = $(this).attr('href');
+                  var posi = $(link).offset().top-50;
+                  $('body,html').stop().animate({scrollTop:posi},700);
+                  return false;
+              });
