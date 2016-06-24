63e25ffa9bee400043ec18ee466dfbb2473d5149
Robeszpierre <robeszpierre@gmail.com>
2016-06-24 23:07:43 +0200

    file directory reworked

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,43 @@
 'use strict';
 
 // Base Gulp File
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
-var compass = require('gulp-compass');
-var useref = require('gulp-useref');
+var gulp        = require('gulp'),
+browserSync = require('browser-sync').create(),
+sass        = require('gulp-sass'),
+compass = require('gulp-compass'),
+useref = require('gulp-useref'),
+gulpif = require('gulp-if'),
+uglify = require('gulp-uglify'),
+minifyCss = require('gulp-minify-css');
 
 // Static Server + watching scss/html files
-gulp.task('serve', function() {
+gulp.task('watch', function() {
 
     browserSync.init({
         server: "./"
     });
 
-    gulp.watch("css/main.css").on('change', browserSync.reload);
-    gulp.watch("*.html").on('change', browserSync.reload);
-    gulp.watch('./scss/**/*.scss', ['compass']);
+    gulp.watch("src/css/main.css").on('change', browserSync.reload);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch('./src/scss/**/*.scss', ['compass']);
 });
 
 gulp.task('compass', function() {
-  gulp.src('./*.scss')
+  gulp.src('./src/*.scss')
     .pipe(compass({
       config_file: './config.rb',
-      css: './css',
-      sass: './scss'
+      css: './src/css',
+      sass: './src/scss'
     }))
     .pipe(gulp.dest('app/assets/temp'));
 });
 
+gulp.task('useref', function () {
+    return gulp.src('src/*.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(gulp.dest(''));
+});
 
-
-gulp.task('default', ['serve']);
+gulp.task('default', ['useref']);
